e5ede94b6ea00b6d3610e4c7f561ca5c538bc6b6
Vladimir Dima <dimavladimir14@gmail.com>
2021-10-22 15:09:23 +0200

    added basic css for carousel and more

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        center: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
 });
+
+// import Swiper JS
+// core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // configure Swiper to use modules
+// Swiper.use([Navigation, Pagination]);
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
